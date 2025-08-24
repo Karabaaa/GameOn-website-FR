80a65f8eeef309658e9067ff37b3fad13d6d013a
Amandine M <amandine.moigno@gmail.com>
2025-08-24 23:21:28 +0200

    feat: validate function to check form inputs before submit (#2)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,7 +20,52 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//function Close Modal
+//function close Modal
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+// validate form :
+function validate(event) {
+  if (event) {
+    event.preventDefault(); // empêche le reload de la page
+  }
+  let formIsValid = true;
+
+  const form = document.forms["reserve"];
+  const first = document.querySelector("#first").value.trim();
+  const last = document.querySelector("#last").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const quantity = document.querySelector("#quantity").value.trim();
+  const location = document.querySelector('input[name="location"]:checked');
+  const conditions = document.querySelector("#checkbox1").checked;
+
+  if (first === null || first.length < 2) {
+    formIsValid = false;
+  }
+
+  if (last === null || last.length < 2) {
+    formIsValid = false;
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    formIsValid = false;
+  }
+
+  if (quantity === "" || isNaN(quantity) || Number(quantity) < 0) {
+    formIsValid = false;
+  }
+
+  if (!location) {
+    formIsValid = false;
+  }
+
+  if (!conditions) {
+    formIsValid = false;
+  }
+
+  if (formIsValid) {
+    form.submit();
+  }
 }
